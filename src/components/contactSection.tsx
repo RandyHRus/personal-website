c56d61253dff5001d9e78e736c03ec6dc1ced6d9
RandyHRus <25328677+RandyHRus@users.noreply.github.com>
2023-05-12 02:52:39 -0700

    Fix contact section header alignment

--- src/components/contactSection.tsx
+++ src/components/contactSection.tsx
@@ -6,7 +6,7 @@ export default function ContactSection() {
     return (
         <div
             id="contact"
-            className="relative flex flex-col w-screen h-screen bg-tertiary p-12 z-30 skew-y-6"
+            className="relative flex flex-col w-screen h-screen pt-12 bg-tertiary z-30 skew-y-6"
         >
             <div className="-skew-y-6 h-full">
                 <Typography variant="h1" className="text-white text-center">
